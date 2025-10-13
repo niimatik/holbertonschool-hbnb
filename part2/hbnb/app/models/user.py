64763babed529b48276768c44b546e68a2599358
niimatik <niimatik.gg@gmail.com>
2025-10-13 16:08:17 +0200

    add: file

---
+++ part2/hbnb/app/models/user.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+from base_class import basemodel
+
+class user(basemodel):
+    def __init__(self, first_name, last_name, email, password, is_admin):
+        super().__init__()
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.__password = password
+        self.is_admin = is_admin
+        self.places = []
+        self.reviews = []
+
+        def add_places(self, place):
+            self.places.append(place)
+
+        def add_reviews(self, review):
+            self.reviews.append(review)
