ec6ade12a64af48ebf5d987f3a23ca617c120e43
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-10-14 10:03:28 +0200

    add: review class created

---
+++ part2/hbnb/app/models/review.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from base_class import basemodel
+
+class review(basemodel):
+    def __init__(self, text, rating, place, user):
+        super().__init__()
+        self.text = text
+        self.rating = rating
+        self.place = place
+        self.user = user
