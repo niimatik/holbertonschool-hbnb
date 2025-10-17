5b61596bd348f1027406a66cb9304aa6851bd327
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-10-17 22:26:36 +0200

    fix: added verifications steps for amenity, place and review entity

--- part2/hbnb/app/models/amenity.py
+++ part2/hbnb/app/models/amenity.py
@@ -5,6 +5,8 @@ from app.models.base_class import basemodel
 class Amenity(basemodel):
     def __init__(self, name):
         super().__init__()
+        if not name:
+            raise ValueError("Name must not be empty !")
         self.name = name
         self.places = []
 
--- part2/hbnb/app/models/review.py
+++ part2/hbnb/app/models/review.py
@@ -5,7 +5,7 @@ from app.models.base_class import basemodel
 class Review(basemodel):
     def __init__(self, text, rating, place_id, user_id):
         super().__init__()
-        if not text:
+        if not text or not rating or not place_id or not user_id:
             raise ValueError("text is empty !")
         self.text = text
         self.rating = rating
