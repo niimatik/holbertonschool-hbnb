fcccdbedb879ab99e82d09e8cad855541a37f910
niimatik <niimatik.gg@gmail.com>
2025-10-17 16:36:25 +0200

    add: file for unit test

---
+++ part2/hbnb/test_hbnb.py
@@ -0,0 +1,224 @@
+import unittest
+from app import create_app
+
+class test_user_endpoint(unittest.TestCase):
+
+    def setUp(self):
+        self.app = create_app()
+        self.client = self.app.test_client()
+
+    def test_create_user(self):
+        response = self.client.post('/api/v1/users/', json={
+            "first_name": "Jane",
+            "last_name": "Doe",
+            "email": "jane.doe@example.com",
+            "is_admin": False
+        })
+        self.assertEqual(response.status_code, 201)
+
+    def test_create_user_invalid_first_name(self):
+        response = self.client.post('/api/v1/users/', json={
+            "first_name": "",
+            "last_name": "doe",
+            "email": "john.doe@exemple.com",
+            "is_admin": False
+        })
+        self.assertEqual(response.status_code, 400)
+
+    def test_create_user_invalid_last_name(self):
+        response = self.client.post('/api/v1/users/', json={
+            "first_name": "john",
+            "last_name": "",
+            "email": "john.doe@exemple.com",
+            "is_admin": False
+        })
+        self.assertEqual(response.status_code, 400)
+
+    def test_create_user_invalid_email(self):
+        response = self.client.post('/api/v1/users/', json={
+            "first_name": "john",
+            "last_name": "doe",
+            "email": "invalid-email",
+            "is_admin": False
+        })
+        self.assertEqual(response.status_code, 400)
+
+    def test_create_user_invalid_is_admin(self):
+        response = self.client.post('/api/v1/users/', json={
+            "first_name": "john",
+            "last_name": "doe",
+            "email": "john.doe@exemple.com",
+            "is_admin": ""
+        })
+        self.assertEqual(response.status_code, 400)
+
+    def test_get_user(self):
+        response = self.client.get('/api/users/')
+        self.assertEqual(response.status_code, 201)
+
+
+class test_amenities_endpoint(unittest.TestCase):
+
+    def setUp(self):
+        self.app = create_app()
+        self.client = self.app.test_client()
+
+    def test_create_amenities(self):
+        response = self.client.post('/api/v1/amenities/', json={
+            "name": "Wi-Fi"
+        })
+        self.assertEqual(response.status_code, 201)
+
+    def test_create_invalide_amenity(self):
+        response = self.client.post('/api/v1/amenities/', json={
+            "name": ""
+        })
+        self.assertEqual(response.status_code, 400)
+
+    def test_get_amenity(self):
+        response = self.client.get('/api/amenities/')
+        self.assertEqual(response.status_code, 201)
+
+
+class test_place_endpoint(unittest.TestCase):
+
+    def setUp(self):
+        self.app = create_app()
+        self.client = self.app.test_client()
+
+    def test_create_places(self):
+        response = self.client.post('/api/v1/palces/', json={
+            "title": "Cozy Apartment",
+            "description": "A nice place to stay",
+            "price": 100.0,
+            "latitude": 37.7749,
+            "longitude": -122.4194,
+            "owner_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+        })
+        self.assertEqual(response.status_code, 201)
+
+    def test_create_invalide_place_title(self):
+        response = self.client.post('/api/v1/places/', json={
+            "title": "",
+            "description": "A nice place to stay",
+            "price": 100.0,
+            "latitude": 37.7749,
+            "longitude": -122.4194,
+            "owner_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+        })
+        self.assertEqual(response.status_code, 400)
+
+    def test_create_invalide_place_description(self):
+        response = self.client.post('/api/v1/places/', json={
+            "title": "Cozy Apartment",
+            "description": "",
+            "price": 100.0,
+            "latitude": 37.7749,
+            "longitude": -122.4194,
+            "owner_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+        })
+        self.assertEqual(response.status_code, 400)
+
+    def test_create_invalide_place_price(self):
+        response = self.client.post('/api/v1/places/', json={
+            "title": "Cozy Apartment",
+            "description": "A nice place to stay",
+            "price": -52.0,
+            "latitude": 37.7749,
+            "longitude": -122.4194,
+            "owner_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+        })
+        self.assertEqual(response.status_code, 400)
+
+    def test_create_invalide_place_latitude(self):
+        response = self.client.post('/api/v1/places/', json={
+            "title": "Cozy Apartment",
+            "description": "A nice place to stay",
+            "price": 100.0,
+            "latitude": 237.7749,
+            "longitude": -122.4194,
+            "owner_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+        })
+        self.assertEqual(response.status_code, 400)
+
+    def test_create_invalide_place_longitude(self):
+        response = self.client.post('/api/v1/places/', json={
+            "title": "Cozy Apartment",
+            "description": "A nice place to stay",
+            "price": 100.0,
+            "latitude": 37.7749,
+            "longitude": -5122.4194,
+            "owner_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+        })
+        self.assertEqual(response.status_code, 400)
+
+    def test_create_invalide_place_owner(self):
+        response = self.client.post('/api/v1/places/', json={
+            "title": "Cozy Apartment",
+            "description": "A nice place to stay",
+            "price": 100.0,
+            "latitude": 37.7749,
+            "longitude": -122.4194,
+            "owner_id": ""
+        })
+        self.assertEqual(response.status_code, 400)
+
+    def test_get_place(self):
+        response = self.client.get('/api/places/')
+        self.assertEqual(response.status_code, 201)
+
+
+class test_review_endpoint(unittest.TestCase):
+
+    def setUp(self):
+        self.app = create_app()
+        self.client = self.app.test_client()
+
+    def test_create_review(self):
+        response = self.client.post('/api/v1/reviews/', json={
+            "text": "Great place to stay!",
+            "rating": 5,
+            "user_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "place_id": "1fa85f64-5717-4562-b3fc-2c963f66afa6"
+        })
+        self.assertEqual(response.status_code, 201)
+
+    def test_create_invalide_review_text(self):
+        response = self.client.post('/api/v1/reviews/', json={
+            "text": "",
+            "rating": 5,
+            "user_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "place_id": "1fa85f64-5717-4562-b3fc-2c963f66afa6"
+        })
+        self.assertEqual(response.status_code, 400)
+
+    def test_create_invalide_review_rating(self):
+        response = self.client.post('/api/v1/reviews/', json={
+            "text": "Great place to stay!",
+            "rating": 9,
+            "user_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "place_id": "1fa85f64-5717-4562-b3fc-2c963f66afa6"
+        })
+        self.assertEqual(response.status_code, 400)
+
+    def test_create_invalide_review_user(self):
+        response = self.client.post('/api/v1/reviews/', json={
+            "text": "Great place to stay!",
+            "rating": 5,
+            "user_id": "",
+            "place_id": "1fa85f64-5717-4562-b3fc-2c963f66afa6"
+        })
+        self.assertEqual(response.status_code, 400)
+
+    def test_create_invalide_review_place(self):
+        response = self.client.post('/api/v1/reviews/', json={
+            "text": "Great place to stay!",
+            "rating": 5,
+            "user_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "place_id": ""
+        })
+        self.assertEqual(response.status_code, 400)
+
+    def test_get_review(self):
+        response = self.client.get('/api/reviews/')
+        self.assertEqual(response.status_code, 201)
